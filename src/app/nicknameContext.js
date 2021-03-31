626b111a2fe237a3e8f809768188e356fd60c9c7
Alexander Mandrikov <mad.nunsez@gmail.com>
2021-03-31 11:06:28 +0700

    change faker generator

--- src/app/nicknameContext.js
+++ src/app/nicknameContext.js
@@ -2,7 +2,7 @@ import faker from 'faker';
 import Cookies from 'js-cookie';
 import { createContext } from 'react';
 
-const nickname = Cookies.get('nickname') ?? faker.name.findName();
+const nickname = Cookies.get('nickname') ?? faker.internet.userName();
 Cookies.set('nickname', nickname, { expires: 1, sameSite: 'strict' });
 
 const NicknameContext = createContext(nickname);
